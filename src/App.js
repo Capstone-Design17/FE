5a321aaee606db824441fbc2f523045478a6ef90
Hyunuk <dksgusdnr17@naver.com>
2023-09-14 06:45:26 +0900

    [tomato-market/plan#3] Prefix 자동으로 추가 설정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          git hook Test <code>src/App.js</code> and save to reload.
+          prefix Test <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
