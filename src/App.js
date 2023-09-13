e0c7ee1549c0c1630a200b5bed18b9c7804e31f6
Hyunuk <dksgusdnr17@naver.com>
2023-09-14 02:50:44 +0900

    [tomato-market/plan#3] git hook test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pull Request Test <code>src/App.js</code> and save to reload.
+          git hook Test <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
