1fc79cb82221a120e0aa327c37c63452c2f1b469
Hyunuk <dksgusdnr17@naver.com>
2023-10-01 07:26:16 +0900

    [tomato-market/plan#12] 로그인, 회원가입 기본 양식 생성

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,13 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <Background>
-      {/* 추가 문구 지정 필요 */}
-      <Link to={'/login'}>
-        <button>시작하기</button>
-      </Link>
+      <div className="homeWrap">
+        {/* 추가 문구 지정 필요 */}
+        <Link to={'/login'} className="homeButton">
+          시작하기
+        </Link>
+        <div className="homeTitle">버튼을 눌러 시작하기</div>
+      </div>
     </Background>
   );
 }
