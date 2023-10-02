595e46b50db9e30df2b7ab4f8d6c9a04873df95a
Hyunuk <dksgusdnr17@naver.com>
2023-10-03 06:57:42 +0900

    [tomato-market/plan#12] [tomato-market/plan#12] axios를 통한 API 요청

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/Login.css';
 import Background from '../components/Background.js';
 import { Link } from 'react-router-dom';
@@ -6,6 +6,39 @@ import { Link } from 'react-router-dom';
 function Login() {
   // 로그인 Form css 지정 및 버튼 위치 지정 필요
   // 로그인한 ID, PW 값을 API 요청 및 Redirect?
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const [idValid, setIdValid] = useState(false);
+  
+  const [pwValid, setPwValid] = useState(false);
+  
+  const handleId = (e) => {
+    setId(e.target.value);
+  };
+  
+  useEffect(() => {
+    const regex = new RegExp('[a-z0-9_-]{6,20}');
+    setIdValid(regex.test(id));
+  }, [id]);
+  
+  const handlePw = (e) => {
+    setPw(e.target.value);
+  };
+  
+  useEffect(() => {
+    const regex = new RegExp('^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,20}$');
+    setPwValid(regex.test(pw));
+  }, [pw]);
+  
+  // 버튼 활성화
+  const allValid = idValid && pwValid;
+  const activeBtn = allValid ? 'undefined' : 'disabled';
+
+  // 로그인
+  const login = () => {
+    console.log(allValid);
+    console.log(id, pw);
+  };
 
   return (
     <Background>
@@ -14,15 +47,20 @@ function Login() {
       <div className="contentWrap">
         <div className="inputTitle">아이디</div>
         <div className="inputWrap">
-          <input className="input" type="text" placeholder="아이디 입력" /> <br />
+          <input className="input" type="text" placeholder="아이디 입력" value={id} onChange={handleId}/> <br />
         </div>
-        <div className="errorMessageWrap">올바른 아이디를 입력해주세요.</div>
+        {!idValid && id.length > 0 && (
+          <div className="errorMessageWrap">올바른 아이디를 입력해주세요.</div>
+        )}
 
         <div className="inputTitle">비밀번호</div>
         <div className="inputWrap">
-          <input className="input" type="text" placeholder="영문, 숫자, 특수문자 포함(8~20자)" /> <br />
+          <input className="input" type="password" placeholder="영문, 숫자, 특수문자 포함(8~20자)" value={pw} onChange={handlePw}/> <br />
         </div>
-        <div className="errorMessageWrap">비밀번호는 영문, 숫자 특수문자를 포함한 8~20자입니다.</div>
+        {!pwValid && pw.length > 0 && (      
+          <div className="errorMessageWrap">비밀번호는 영문, 숫자 특수문자를 포함한 8~20자입니다.</div>
+        )}
+
 
         <div className="linksWrap">
           <Link to={'/signup'}>회원가입</Link>
@@ -35,7 +73,7 @@ function Login() {
       </div>
 
       <div>
-        <button disabled={true} className="bottomButton">
+        <button className={`bottomButton ${activeBtn}`} onClick={login}>
           로그인
         </button>
       </div>
