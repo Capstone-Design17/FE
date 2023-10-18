74d14574f1eb17cd417c27f1f68d15e95a50674d
Hyunuk <dksgusdnr17@naver.com>
2023-10-19 08:46:42 +0900

    [tomato-market/plan#7] 문법 수정

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -29,30 +29,31 @@ export default function Board() {
       url: 'api/board/getPostList',
       method: 'get',
     })
-    .then((response) => {
-      console.log('Get List');
-      console.log(response);
-      if (response.status === 200) {
-        // 로컬은 어떻게?
+      .then((response) => {
+        console.log('Get List');
+        console.log(response);
+        if (response.status === 200) {
+          // 로컬은 어떻게?
 
-        // [postList, imageList]를 담은 리스트 생성
-        const post = response.data.postList;
-        const image = response.data.imageList;
-        if (post && image && post.length === image.length) {
-          const mergedList = response.data.postList.map((postItem, index) => ({
-            ...postItem,
-            image: image[index],
-          }));
-          setPostList(mergedList);
-          console.log(postList);
+          // [postList, imageList]를 담은 리스트 생성
+          const post = response.data.postList;
+          const image = response.data.imageList;
+          if (post && image && post.length === image.length) {
+            const mergedList = response.data.postList.map((postItem, index) => ({
+              ...postItem,
+              image: image[index],
+            }));
+            setPostList(mergedList);
+            console.log(postList);
+          }
+          setIsLoading(false);
+        } else {
+          throw new Error('데이터 불러오기 실패');
         }
-        setIsLoading(false);
-      }
-      else {
-        throw new Error("데이터 불러오기 실패");
-      }
-    })
-    .catch((error) => {alert(error);});
+      })
+      .catch((error) => {
+        alert(error);
+      });
   }, [isLoading]);
 
   return (
@@ -76,7 +77,7 @@ export default function Board() {
               const imageUrl = '/image/' + post.image.uuid; // 운영 환경의 url
               return (
                 <Card key={index} sx={{ display: 'flex', margin: '5px 0', height: '130px' }}>
-                  <CardMedia component="img" sx={{ maxWidth: 130, maxHeight:130, overflow: 'hidden' }} image={imageUrl} alt="default" />
+                  <CardMedia component="img" sx={{ maxWidth: 130, maxHeight: 130, overflow: 'hidden' }} image={imageUrl} alt="default" />
                   <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
                     <CardContent sx={{ flex: '1 0 auto', p: 1 }}>
                       <Typography component="div" variant="h6">
@@ -106,9 +107,9 @@ export default function Board() {
           </div>
         ) : (
           <p>No Data</p>
-          )}
+        )}
 
-        <div style={{ width: '100%', textAlign: 'center', padding: '20px 0', }}>페이징 예정 1 2 3</div>
+        <div style={{ width: '100%', textAlign: 'center', padding: '20px 0' }}>페이징 예정 1 2 3</div>
         {/* Bottom Navbar */}
         <Link to={'/post'}>
           <Fab
