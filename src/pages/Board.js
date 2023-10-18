6991e38f8a896a57d9e42c06b2a88df375672394
Hyunuk <dksgusdnr17@naver.com>
2023-10-18 11:06:34 +0900

    [tomato-market/plan#7] 거래 목록 출력

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Background from 'components/Background';
@@ -9,7 +9,6 @@ import EditIcon from '@mui/icons-material/Edit';
 import { Box, Typography } from '@mui/material';
 import 'styles/Board.css';
 import axios from 'axios';
-
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -21,10 +20,11 @@ export default function Board() {
     setUserId(id);
   };
 
-  const [testImage, setTestImage] = useState();
-  let [imageUrl, setImageUrl] = useState();
+  const [postList, setPostList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const getList = () => {
+  // const getList = () => {
+  useEffect(() => {
     axios({
       url: 'api/board/getPostList',
       method: 'get',
@@ -32,67 +32,77 @@ export default function Board() {
       console.log('Get List');
       console.log(response);
       if (response.status === 200) {
-        console.log(response.data.imageList);
-        console.log(response.data.imageList[0]);
-        console.log(response.data.imageList[0].uuid);
-        setTestImage(response.data.imageList[0].uuid);
-        setImageUrl('/image/' + testImage); // nginx : /image 경로로 오는 요청 Proxy
         // 로컬은 어떻게?
-        console.log(imageUrl);
+
+        // [postList, imageList]를 담은 리스트 생성
+        const post = response.data.postList;
+        const image = response.data.imageList;
+        if (post && image && post.length === image.length) {
+          const mergedList = response.data.postList.map((postItem, index) => ({
+            ...postItem,
+            image: image[index],
+          }));
+          setPostList(mergedList);
+          console.log(postList);
+        }
+        setIsLoading(false);
       }
     });
-  };
+  }, [isLoading]);
 
   return (
     <Background>
       <Navbar getUserId={getUserId} userId={userId} />
       {/* 등록된 아이템 리스트 보여주기 */}
       <div className="contentWrap">
-        아이템 리스트가 보여질 화면입니다.
-        <div onClick={getList}>Test Click</div>
+        <h3 style={{ marginTop: 0 }}>등록된 거래 목록</h3>
         {/* 이미지 접근 예시 */}
         {/* nginx proxy로 접근 */}
         {/* <img src='http://localhost:80/image/default.png' style={{width: '120px', marginBottom: '10px'}}/> */}
-        {/* Card */}
-        <Card sx={{ display: 'flex', margin: '5px 0' }}>
-          <CardMedia component="img" sx={{ maxWidth: 140, overflow: 'hidden' }} image="http://localhost:80/image/default.png" alt="default" />
-          <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-            <CardContent sx={{ flex: '1 0 auto' }}>
-              <Typography component="div" variant="h6">
-                Post title
-              </Typography>
-              <Typography variant="subtitle1" color="text.secondary" component="div">
-                location
-              </Typography>
-            </CardContent>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2 }}>
-              <Typography variant="h6">10000원</Typography>
-              <Typography variant="overline" color={'error'} sx={{ border: 1, borderRadius: 1, textAlign: 'center', width: '60px', height: '30px' }}>
-                판매완료
-              </Typography>
-            </Box>
-          </Box>
-        </Card>
-        {/* Card2 */}
-        <Card sx={{ display: 'flex', margin: '5px 0' }}>
-          <CardMedia component="img" sx={{ maxWidth: 140, overflow: 'hidden' }} image="http://localhost:80/image/test.png" alt="default" />
-          <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
-            <CardContent sx={{ flex: '1 0 auto' }}>
-              <Typography component="div" variant="h6">
-                Post title2
-              </Typography>
-              <Typography variant="subtitle1" color="text.secondary" component="div">
-                location2
-              </Typography>
-            </CardContent>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2 }}>
-              <Typography variant="h6">10000원</Typography>
-              <Typography variant="overline" color={'green'} sx={{ border: 1, borderRadius: 1, textAlign: 'center', width: '60px', height: '30px' }}>
-                판매중
-              </Typography>
-            </Box>
-          </Box>
-        </Card>
+
+        {/* 반복 Card 구조*/}
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : postList.length > 0 ? (
+          <div style={{ overflow: 'scroll' }}>
+            {postList.map((post, index) => {
+              // index를 수정?
+              const imageUrl = 'http://localhost:80/image/' + post.image.uuid;
+              return (
+                <Card key={index} sx={{ display: 'flex', margin: '5px 0', height: '140px' }}>
+                  <CardMedia component="img" sx={{ maxWidth: 140, overflow: 'hidden' }} image={imageUrl} alt="default" />
+                  <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%' }}>
+                    <CardContent sx={{ flex: '1 0 auto', p: 1 }}>
+                      <Typography component="div" variant="h6">
+                        {post.title}
+                      </Typography>
+                      <Typography variant="caption" color="text.secondary" component="div">
+                        {post.location}
+                      </Typography>
+                    </CardContent>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 1 }}>
+                      <Typography variant="h6">{post.price}원</Typography>
+                      {post.status === 0 ? (
+                        <Typography variant="overline" color={'green'} sx={{ border: 1, borderRadius: 1, textAlign: 'center', width: '60px', height: '30px' }}>
+                          판매중
+                          {/* post.status에 따라 다른 값 : 0 or 1? */}
+                        </Typography>
+                      ) : (
+                        <Typography variant="overline" color={'error'} sx={{ border: 1, borderRadius: 1, textAlign: 'center', width: '60px', height: '30px' }}>
+                          판매완료
+                        </Typography>
+                      )}
+                    </Box>
+                  </Box>
+                </Card>
+              );
+            })}
+            <div style={{ width: '100%', textAlign: 'center', padding: '20px 0' }}>페이징 예정 1 2 3</div>
+          </div>
+        ) : (
+          <p>No Data</p>
+        )}
+
         {/* Bottom Navbar */}
         <Link to={'/post'}>
           <Fab
